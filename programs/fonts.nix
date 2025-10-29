f8d0da3c0e0378f14244fdc6c88b7b63a10537cd
Hutch <will@thehutchery.com>
2025-10-29 08:53:05 -0400

    fix font

--- programs/fonts.nix
+++ programs/fonts.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
-    noto-fonts-emoji
+    noto-fonts-color-emoji
     font-awesome # For icons in waybar, etc.
     nerd-fonts.space-mono
   ];
