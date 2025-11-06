a959a4a3414f076d58470625a376916935853f7b
Hutch <will@thehutchery.com>
2025-11-06 08:23:02 -0500

    add goose-cli

--- modules/dev-tools.nix
+++ modules/dev-tools.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ../programs/pulumi.nix ../programs/jujutsu.nix ];
-  home.packages = with pkgs; [ awscli2 doppler docker-compose cargo-lambda claude-code ];
+  imports = [
+    ../programs/pulumi.nix
+    ../programs/jujutsu.nix
+  ];
+  home.packages = with pkgs; [
+    awscli2
+    doppler
+    docker-compose
+    cargo-lambda
+    claude-code
+    goose-cli
+  ];
 }
