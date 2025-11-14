229432eff226a65f5b02151fd668b84084cb599a
Hutch <will@thehutchery.com>
2025-11-14 09:56:53 -0500

    temp

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -6,12 +6,24 @@
     ./hardware-configuration.nix
   ];
 
-  nix.settings.download-buffer-size = 536870912; # 512 MB (default is 64 MB)
+  nix.settings = {
+    download-buffer-size = 536870912; # 512 MB (default is 64 MB)
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   # Use the latest kernel for better AMD support
   boot.kernelPackages = pkgs.linuxPackages_latest;
