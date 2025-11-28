4a828855844be0327ca602fd29d59f10a4befe13
Hutch <hutch@macro.com>
2025-11-28 12:52:52 -0500

    fix jj signing

--- programs/jujutsu.nix
+++ programs/jujutsu.nix
@@ -8,6 +8,11 @@
         email = if config.home.username == "work" then "hutch@macro.com" else "will@thehutchery.com";
         name = "Hutch";
       };
+      signing = {
+        sign-all = true;
+        backend = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENMDh7q29md/cAQWBp13Fk//buN4KiQIiwJze+rRj9P";
+      };
     };
   };
 }
