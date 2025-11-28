193ccd0c3e8801fa00ccc85e67b1d885d18c5770
Hutch <hutch@macro.com>
2025-11-28 13:16:17 -0500

    fix jj auto-sign

--- programs/jujutsu.nix
+++ programs/jujutsu.nix
@@ -9,6 +9,7 @@
         name = "Hutch";
       };
       signing = {
+        behavior = "own";
         sign-all = true;
         backend = "ssh";
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENMDh7q29md/cAQWBp13Fk//buN4KiQIiwJze+rRj9P";
