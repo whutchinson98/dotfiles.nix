2c060b64976060371f432410ce5ac9408a30c963
Hutch <hutch@macro.com>
2025-12-08 13:32:29 -0500

    jj settings

--- programs/jujutsu.nix
+++ programs/jujutsu.nix
@@ -14,6 +14,14 @@
         backend = "ssh";
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENMDh7q29md/cAQWBp13Fk//buN4KiQIiwJze+rRj9P";
       };
+      remotes = {
+        origin = {
+          auto-track-bookmarks = "glob:hutch/*";
+        };
+        upstream = {
+          auto-track-bookmarks = "main";
+        };
+      };
     };
   };
 }
